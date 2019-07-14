bdc734317c059e5f04136a46ecd6fb4fc229a988
Jherom <jherom.chacon@gmail.com>
2019-07-14 16:37:52 -0600

    Adding a controller for the fidelity Card

---
+++ src/controllers/CardController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import entities.FidelityCard;
+
+/**
+ *
+ * @author Jheron Chacon
+ */
+public class CardController extends Controller{
+
+    public CardController(FidelityCard entity) {
+        super(entity);
+    }
+
+    @Override
+    protected void menuAdd() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void menuUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void menuDelete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void menuReport() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
